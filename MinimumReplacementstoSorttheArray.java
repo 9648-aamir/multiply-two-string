f763c293980f00afb2cba7175d4c2674af7142d6
9648-aamir <aamir9648770208@gmail.com>
2023-08-30 23:12:11 +0530

    gg

---
+++ MinimumReplacementstoSorttheArray.java
@@ -0,0 +1,19 @@
+public class MinimumReplacementstoSorttheArray {
+    public long minimumReplacement(int[] nums) {
+        int n = nums.length;
+        long ops = 0;
+        int prev = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            int k = nums[i] / prev;
+
+            if ((nums[i]) % prev != 0) {
+                k++;
+                prev = nums[i] / k;
+            }
+            ops += k - 1;
+        }
+
+        return ops;
+    }
+}
