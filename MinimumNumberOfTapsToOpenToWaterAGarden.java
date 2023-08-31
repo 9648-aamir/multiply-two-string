4e96295c3ab0c55b8e1a8d3ff6b50e4538386b10
9648-aamir <aamir9648770208@gmail.com>
2023-08-31 20:26:31 +0530

    aaa

---
+++ MinimumNumberOfTapsToOpenToWaterAGarden.java
@@ -0,0 +1,25 @@
+public class MinimumNumberOfTapsToOpenToWaterAGarden {
+    public int minTaps(int n, int[] ranges) {
+        int[] tap = new int[n+1];
+        for(int i=0; i<=n; i++) {
+            if(ranges[i] == 0) {
+                continue;
+            }
+            int left = Math.max(0, i-ranges[i]);
+            tap[left] = Math.max(tap[left], i+ranges[i]);
+        }
+
+        int curEnd=0, farthest=0, total=0;
+        for(int i=0; i<n+1 && curEnd<n; curEnd=farthest) {
+            total++;
+            while(i<n+1 && i<=curEnd) {
+                farthest = Math.max(farthest, tap[i++]);
+            }
+            if(curEnd == farthest) {
+                return -1;
+            }
+        }
+        return total;
+    }
+    
+}
