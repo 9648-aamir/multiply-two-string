542f86151b9f75c5eb9c5e8e4f8ee1733d03f762
9648-aamir <aamir9648770208@gmail.com>
2023-09-28 22:14:46 +0530

    hjkgh

--- Main.java
+++ Main.java
@@ -1,32 +1,47 @@
-import java.util.PriorityQueue;
-
 public class Main {
-    public static void main(String[] args) {
-        int[] arr = {3, 1, 4, 2, 5, 6};
-        int k = 3; // Change K to find the Kth largest element
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
 
-        int kthLargest = findKthLargest(arr, k);
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-        System.out.println("The " + k + "th largest element is: " + kthLargest);
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Check if the left half is sorted
+            if (nums[left] <= nums[mid]) {
+                // Check if the target is in the left half
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // If the left half is not sorted, then the right half must be sorted
+            else {
+                // Check if the target is in the right half
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1; // Element not found
     }
 
-    public static int findKthLargest(int[] nums, int k) {
-        if (nums == null || k < 1 || k > nums.length)
-            throw new IllegalArgumentException("Invalid input");
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 5;
+        int index = search(nums, target);
 
-        // Create a Max Heap (Priority Queue)
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-
-        // Add elements from the array to the Max Heap
-        for (int num : nums) {
-            maxHeap.offer(num);
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array.");
         }
-
-        // Extract the Kth largest element from the Max Heap
-        for (int i = 1; i < k; i++) {
-            maxHeap.poll(); // Remove the largest (K-1) elements
-        }
-
-        return maxHeap.peek(); // Return the Kth largest element
     }
 }
