8b2f3cae0b612311cd4b195175e41307069fd4d4
9648-aamir <aamir9648770208@gmail.com>
2023-10-02 22:57:21 +0530

    SDFGHJ

--- Main.java
+++ Main.java
@@ -1,85 +1,33 @@
-class TreeNode {
-    int data;
-    TreeNode left;
-    TreeNode right;
+public class Main{
+    public static int  jmpSearch(int arr[], int target){
+        int n = arr.length;
+        int step=(int)Math.floor(Math.sqrt(n));
 
-    public TreeNode(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
+        int prev=0;
 
-class BinarySearchTree {
-    private TreeNode root;
-
-    public BinarySearchTree() {
-        root = null;
-    }
-
-    // Insert a value into the BST
-    public void insert(int value) {
-        root = insertRec(root, value);
-    }
-
-    // Helper method to recursively insert a value into the BST
-    private TreeNode insertRec(TreeNode root, int value) {
-        if (root == null) {
-            root = new TreeNode(value);
-            return root;
+        while(arr[Math.min(step, n)-1]<target){
+            prev=step;
+            step+=(int )Math.floor(Math.sqrt(n));
+            if(prev>=n){
+                return -1;
+            }
         }
-
-        if (value < root.data) {
-            root.left = insertRec(root.left, value);
-        } else if (value > root.data) {
-            root.right = insertRec(root.right, value);
+        for(int i=prev;i<Math.min(step, n);i++){
+            if(arr[i]==target){
+                return i;
+            }
         }
-
-        return root;
+        return -1;
     }
-
-    // Search for a value in the BST
-    public boolean search(int value) {
-        return searchRec(root, value);
-    }
-
-    // Helper method to recursively search for a value in the BST
-    private boolean searchRec(TreeNode root, int value) {
-        if (root == null) {
-            return false; // Element not found
-        }
-
-        if (root.data == value) {
-            return true; // Element found
-        }
-
-        if (value < root.data) {
-            return searchRec(root.left, value);
-        } else {
-            return searchRec(root.right, value);
-        }
-    }
-}
-
-public class Main {
     public static void main(String[] args) {
-        BinarySearchTree bst = new BinarySearchTree();
+        int arr[]={1,2,3,4,5,6,7,8,9};
+        int target = 10;
 
-        // Insert elements into the BST
-        bst.insert(50);
-        bst.insert(30);
-        bst.insert(70);
-        bst.insert(20);
-        bst.insert(40);
-        bst.insert(60);
-        bst.insert(80);
-
-        // Search for elements in the BST
-        int targetElement = 40;
-        if (bst.search(targetElement)) {
-            System.out.println("Element " + targetElement + " found in the BST.");
-        } else {
-            System.out.println("Element " + targetElement + " not found in the BST.");
+        int result = jmpSearch(arr, target);
+        if(result!=1){
+            System.out.println("Element at the index:"+result);
+        }else{
+            System.out.println("not found");
         }
     }
 }
