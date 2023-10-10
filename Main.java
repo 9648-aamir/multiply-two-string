bbfdf4b1f61fd59ba61fce2df34b14582a1d195b
9648-aamir <aamir9648770208@gmail.com>
2023-10-10 11:27:16 +0530

    FGHJK

--- Main.java
+++ Main.java
@@ -1,42 +1,26 @@
+import java.util.*;
 public class Main {
-    public static int interpolationSearch(int[] arr, int target) {
-        int low = 0;
-        int high = arr.length - 1;
-
-        while (low <= high && target >= arr[low] && target <= arr[high]) {
-            if (low == high) {
-                if (arr[low] == target) {
-                    return low;
-                }
-                return -1; // Target not found
-            }
-
-            // Calculate the estimated position using interpolation formula
-            int pos = low + ((target - arr[low]) * (high - low)) / (arr[high] - arr[low]);
-
-            if (arr[pos] == target) {
-                return pos;
-            }
-
-            if (arr[pos] < target) {
-                low = pos + 1;
-            } else {
-                high = pos - 1;
-            }
+    public static int exponetialSearch(int[] arr, int target) {
+        int n=arr.length;
+        if(n==arr[0]){
+            return 0;
         }
-
-        return -1; // Target not found
-    }
-
-    public static void main(String[] args) {
-        int[] sortedArray = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21};
-        int targetElement = 13;
-        int result = interpolationSearch(sortedArray, targetElement);
-
-        if (result != -1) {
-            System.out.println("Element " + targetElement + " found at index " + result);
-        } else {
-            System.out.println("Element " + targetElement + " not found in the array");
+        int i=1;
+        while(i<n && arr[i]<=target){
+            i*=2;
         }
+    return Arrays.binarySearch(arr,i/2,Math.min(i, n),target);
+}
+public static void main(String[] args) {
+    int arr[]={1,3,5,7,9,11,13,14,15};
+    int target = 13;
+
+    int result = exponetialSearch(arr, target);
+
+    if(result>=0){
+        System.out.println("Element at the index:"+result);
+    }else{
+        System.out.println("Element is not found ");
     }
+}
 }
