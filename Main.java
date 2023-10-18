edfb0b3d190199b49c8413fdcb3d7647de13eeab
9648-aamir <aamir9648770208@gmail.com>
2023-10-18 23:45:38 +0530

    DFGHJ

--- Main.java
+++ Main.java
@@ -1,17 +1,76 @@
-import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    Node head;
+
+    // Insert a new node at the end of the list
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    // Delete a node with a specific value from the list
+    public void delete(int value) {
+        if (head == null) {
+            return;
+        }
+        if (head.data == value) {
+            head = head.next;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            if (current.next.data == value) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    // Traverse and print the linked list
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-    Set<String>hashSet=new HashSet<>();
+        SinglyLinkedList myList = new SinglyLinkedList();
 
-    hashSet.add("Aamir");
-    hashSet.add("Danish");
-    hashSet.add("Tabish");
+        myList.insert(1);
+        myList.insert(2);
+        myList.insert(3);
+        myList.insert(4);
 
-    String target="Danish";
-    if(hashSet.contains(target)){
-        System.out.println("Element "+target+" exits in the hashSet");
-    }else{
-        System.out.println("Element "+target+" not exits in the hashSet");
-    }
+        System.out.println("Original Linked List:");
+        myList.display();
+
+        myList.delete(2);
+
+        System.out.println("Linked List after deleting 2:");
+        myList.display();
     }
 }
