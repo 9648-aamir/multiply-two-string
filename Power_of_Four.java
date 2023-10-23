6bd02742ed6cfecf26623cef31eb5e935985aae6
9648-aamir <aamir9648770208@gmail.com>
2023-10-23 23:39:22 +0530

    GHJK

---
+++ Power_of_Four.java
@@ -0,0 +1,14 @@
+public class Power_of_Four {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n > 1) {
+            if (n % 4 != 0) {
+                return false;
+            }
+            n /= 4;
+        }
+        return true;
+    }
+}
