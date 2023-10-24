65af52ce9c81d1f7e4af582ae6562a3594e7817e
9648-aamir <aamir9648770208@gmail.com>
2023-10-24 23:18:22 +0530

    dfghj

---
+++ FindLargestValue_inEachTreeRow.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+public class FindLargestValue_inEachTreeRow {
+    public List<Integer> largestValues(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        queue.offer(root);
+        if(root==null)  return ans;
+        while(!queue.isEmpty())
+        {
+            int level = queue.size();
+            int max = Integer.MIN_VALUE;
+            for(int i=0;i<level;i++)
+            {
+                TreeNode temp =queue.poll();
+                if(temp.left!=null)
+                {
+                    queue.offer(temp.left);
+                }
+                if(temp.right!=null)
+                {
+                    queue.offer(temp.right);
+                }
+                max= Math.max(max, temp.val);
+            }
+            ans.add(max);
+        }
+        return ans;
+    }
+}
