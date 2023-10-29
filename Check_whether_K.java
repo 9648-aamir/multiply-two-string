79d25c0ca3799b343b712f5b334a5a2e8dc73d32
9648-aamir <aamir9648770208@gmail.com>
2023-10-29 22:37:02 +0530

    DFGHJ

---
+++ Check_whether_K.java
@@ -0,0 +1,17 @@
+public class Check_whether_K{
+    
+    // Function to check if Kth bit is set or not.
+    static boolean checkKthBit(int n, int k)
+    {
+        // Your code here
+        return ((n>>k)&1)==1;
+    }
+
+
+    // Function to check if Kth bit is set or not.
+    static boolean checkKthBit(int n, int k)
+    {
+        // Your code here
+        return ((n>>k)&1)==1;
+    }
+}
