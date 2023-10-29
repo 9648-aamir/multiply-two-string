5f7707d16041a9709d000ecc7fc7c8c55233e035
9648-aamir <aamir9648770208@gmail.com>
2023-10-29 22:38:05 +0530

    DFGHJ

---
+++ PoorPigs.java
@@ -0,0 +1,12 @@
+public class PoorPigs {
+    public int poorPigs(int buckets, int tdie, int ttest) {
+        int test_val = ttest/tdie;
+        int count=0;
+        
+        while( Math.pow(test_val+1,count) < buckets ){
+            count++;
+        }
+        
+        return count;
+    }
+}
