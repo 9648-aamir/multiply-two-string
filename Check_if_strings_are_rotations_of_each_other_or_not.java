6cfd3c88c2867d699e65acedf6039f35b58366ef
9648-aamir <aamir9648770208@gmail.com>
2023-11-14 23:22:55 +0530

    FGHJ

--- Check_if_strings_are_rotations_of_each_other_or_not.java
+++ Check_if_strings_are_rotations_of_each_other_or_not.java
@@ -1,3 +1,6 @@
 public class Check_if_strings_are_rotations_of_each_other_or_not {
-    
+    public static boolean areRotations(String s1, String s2 ){
+        s1 = s1 + s1;
+        return s1.contains(s2);
+    }
 }
---
+++ UniqueLength3PalindromicSubsequences.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueLength3PalindromicSubsequences {
+    public int countPalindromicSubsequence(String s) {
+        int res = 0;
+        Set<Character> uniq = new HashSet<>();
+
+        for (char c : s.toCharArray()) {
+            uniq.add(c);
+        }
+
+        for (char c : uniq) {
+            int start = s.indexOf(c);
+            int end = s.lastIndexOf(c);
+
+            if (start < end) {
+                Set<Character> charSet = new HashSet<>();
+                for (int i = start + 1; i < end; i++) {
+                    charSet.add(s.charAt(i));
+                }
+                res += charSet.size();
+            }
+        }
+
+        return res;        
+    }
+}
