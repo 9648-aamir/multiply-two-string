b047511949aae4c9cd525877f1b6a49a6ab8018d
9648-aamir <aamir9648770208@gmail.com>
2023-11-19 22:51:05 +0530

    FGHJ

---
+++ ReductionOperations_toMake_theArrayElementsEqual.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class ReductionOperations_toMake_theArrayElementsEqual {
+    public int reductionOperations(int[] nums) {
+        Arrays.sort(nums);
+        int si=nums.length;
+        int ans=0;
+        for(int i=nums.length-1;i>0;i--){
+            if(nums[i-1]!=nums[i]){
+                ans+=si-i;
+            }
+        }
+        return ans;
+    }
+}
