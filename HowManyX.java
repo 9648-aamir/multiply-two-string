1e0d6cff46f60fb62fdb43c7da1046d286798063
9648-aamir <aamir9648770208@gmail.com>
2023-12-06 23:30:53 +0530

    NMK

---
+++ HowManyX.java
@@ -0,0 +1,15 @@
+public class HowManyX {
+     int countX(int L, int R, int X) {
+        // code here
+        int cnt=0;
+        for(int i=L+1;i<R;i++){
+            int temp=i;
+            while(temp>0){
+                int digit=temp%10;
+                if(digit==X) cnt++;
+                temp=temp/10;
+            }
+        }
+        return cnt;
+    }
+}
