d232c4c94f148e4b63c9fba3d9e79d0d8051a330
9648-aamir <aamir9648770208@gmail.com>
2023-12-09 23:59:37 +0530

    GHJK

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+
+        inorder(root, res);
+        return res;        
+    }
+
+    private void inorder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, res);
+        res.add(node.val);
+        inorder(node.right, res);
+    }   
+}
