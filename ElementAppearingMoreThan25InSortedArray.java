9d3c93cacc463a81eb27315c52a1a601eef972a3
9648-aamir <aamir9648770208@gmail.com>
2023-12-11 23:36:29 +0530

    GH

---
+++ ElementAppearingMoreThan25InSortedArray.java
@@ -0,0 +1,14 @@
+public class ElementAppearingMoreThan25InSortedArray {
+    public int findSpecialInteger(int[] arr) {
+        int n = arr.length;
+        int quarter = n / 4;
+
+        for (int i = 0; i < n - quarter; i++) {
+            if (arr[i] == arr[i + quarter]) {
+                return arr[i];
+            }
+        }
+
+        return -1;
+    }
+}
