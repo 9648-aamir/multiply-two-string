6ea433506b73f3b846f5fcd942d98f2843b2cdec
9648-aamir <aamir9648770208@gmail.com>
2023-12-21 22:05:12 +0530

    FGHJ

--- Main.java
+++ Main.java
@@ -1,76 +1,9 @@
+import java.util.Scanner;
 
-class Node {
-    int data;
-    Node next;
 
-    public Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
 
-class SinglyLinkedList {
-    Node head;
+public class Main{
+    Scanner sc= new Scanner(System.in);
 
-    // Insert a new node at the end of the list
-    public void insert(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-        } else {
-            Node current = head;
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
-    // Delete a node with a specific value from the list
-    public void delete(int value) {
-        if (head == null) {
-            return;
-        }
-        if (head.data == value) {
-            head = head.next;
-            return;
-        }
-        Node current = head;
-        while (current.next != null) {
-            if (current.next.data == value) {
-                current.next = current.next.next;
-                return;
-            }
-            current = current.next;
-        }
-    }
-
-    // Traverse and print the linked list
-    public void display() {
-        Node current = head;
-        while (current != null) {
-            System.out.print(current.data + " -> ");
-            current = current.next;
-        }
-        System.out.println("null");
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        SinglyLinkedList myList = new SinglyLinkedList();
-
-        myList.insert(1);
-        myList.insert(2);
-        myList.insert(3);
-        myList.insert(4);
-
-        System.out.println("Original Linked List:");
-        myList.display();
-
-        myList.delete(2);
-
-        System.out.println("Linked List after deleting 2:");
-        myList.display();
-    }
+System.out
 }
