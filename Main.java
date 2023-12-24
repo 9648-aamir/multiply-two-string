736ac829173feef19a257a317a665df6d8beaf04
9648-aamir <aamir9648770208@gmail.com>
2023-12-24 23:19:11 +0530

    FGH

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
-import java.util.Scanner;
+public class Main {
+    public static void findMinMax(int arr[]) {
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                continue;
+            }
+            System.out.print(" "+arr[i]);
 
-
-
-public class Main{
-    Scanner sc= new Scanner(System.in);
-
-System.out
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>0){
+                continue;
+            }
+            System.out.print(" "+arr[i]);
+        }
+        
+    }
+    public static void main(String[] args) {
+        int arr[]={2,0,5,6,1,0,1,0};
+        findMinMax(arr);
+    }
 }
