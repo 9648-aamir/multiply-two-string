92ad4e8993c9f442481105e9d5f61c5c866e6b6d
9648-aamir <aamir9648770208@gmail.com>
2023-12-25 22:41:46 +0530

    FGH

--- Main.java
+++ Main.java
@@ -1,22 +1,19 @@
+import java.util.*;
+
 public class Main {
     public static void findMinMax(int arr[]) {
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==0){
-                continue;
-            }
-            System.out.print(" "+arr[i]);
-
-        }
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>0){
-                continue;
-            }
-            System.out.print(" "+arr[i]);
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
         }
         
+        Collections.sort(list);
+        System.out.println(list);
     }
+
     public static void main(String[] args) {
-        int arr[]={2,0,5,6,1,0,1,0};
+        int arr[] = {1, 4, 2, 6, 3, 8, 5, 9, 7};
         findMinMax(arr);
     }
 }
