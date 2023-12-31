4623cbf97005b110db30ebdc95d1cd33e5b679ab
9648-aamir <aamir9648770208@gmail.com>
2023-12-31 23:05:39 +0530

    WERTYUI

---
+++ LargestSubstringBetweenTwoEqualCharacters.java
@@ -0,0 +1,12 @@
+public class LargestSubstringBetweenTwoEqualCharacters {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        int current = s.length()-1;
+        for (int i=s.length()-1; i>0;i--){
+            for (int j=0; j+i<s.length(); j++){
+                if (s.charAt(j) == s.charAt(j+i)) return i-1;
+            }
+            current--;
+        }
+        return -1;
+    }
+}
