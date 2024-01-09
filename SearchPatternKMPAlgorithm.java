a6509a0135fe3aae18947aec1a9605f5969a1be7
9648-aamir <aamir9648770208@gmail.com>
2024-01-09 23:59:57 +0530

    FGHIJHJJ

---
+++ SearchPatternKMPAlgorithm.java
@@ -0,0 +1,13 @@
+public class SearchPatternKMPAlgorithm {
+    ArrayList<Integer> search(String pat, String txt)
+    {
+        // your code here
+        ArrayList<Integer>A=new ArrayList<>();
+        int index=txt.indexOf(pat);
+        while(index!=-1){
+            A.add(index+1);
+            index=txt.indexOf(pat,index+1);
+        }
+        return A;
+    }
+}
