248e9a046337192ec0423ef9a81b63863a882608
9648-aamir <aamir9648770208@gmail.com>
2024-01-24 22:25:07 +0530

    KJHJJJKJ

--- Main.java
+++ Main.java
@@ -1,19 +1,30 @@
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
-    public static void findMinMax(int arr[]) {
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        for (int i = 0; i < arr.length; i++) {
-            list.add(arr[i]);
-        }
-        
-        Collections.sort(list);
-        System.out.println(list);
-    }
-
     public static void main(String[] args) {
-        int arr[] = {1, 4, 2, 6, 3, 8, 5, 9, 7};
-        findMinMax(arr);
+        int arr[] = {6, 2, 4, 3, 8, 1, 7, 9, 0};
+
+        int mid = arr.length / 2;
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        // Sort the last half of the array
+        Arrays.sort(arr, mid, arr.length);
+
+        // Invert the last half of the array
+        for (int i = mid, j = arr.length - 1; i < j; i++, j--) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        System.out.println();
+
+        // Print the modified array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
