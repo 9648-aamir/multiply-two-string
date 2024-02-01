61876d0af78faf88acd93a9b0009afc4a1caf196
9648-aamir <aamir9648770208@gmail.com>
2024-02-01 22:08:57 +0530

    guyu

---
+++ patern.java
@@ -0,0 +1,15 @@
+public class patern {
+    public static void main(String[] args) {
+        Integer num1=100;
+        Integer num2=100;
+        Integer num3=300;
+        Integer num4=300;
+
+        if(num1==num2){
+            System.out.println("num1==num2");
+        }else{ System.out.println("num1!=num2");}
+        if(num3==num4){
+            System.out.println("num3==num4");
+        }else{ System.out.println("num3!=num4");}
+    }
+}
