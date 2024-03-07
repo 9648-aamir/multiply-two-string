ce7aeaba4c38b425349f356e2b4dae032bad1036
9648-aamir <aamir9648770208@gmail.com>
2024-03-07 11:08:09 +0530

    KJJJJJKMN

---
+++ SearchPatternRabinKarpAlgorithm.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class SearchPatternRabinKarpAlgorithm {
+    ArrayList<Integer> search(String pattern, String text) {
+        // your code here
+        int n = text.length();
+        int m = pattern.length();
+        ArrayList<Integer> idx = new ArrayList<>();
+        for (int i = 0; i < n - m + 1; i++) {
+            String temp = text.substring(i, m + i);
+            if (temp.equals(pattern)) {
+                idx.add(i + 1);
+            }
+        }
+        return idx;
+    }
+}
